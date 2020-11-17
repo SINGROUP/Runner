21bb08fc0a6c902763a254dd2e7343012c1ed756
Yashasvi Ranawat <yashasvi.ranawat@gmail.com>
2020-11-17 03:38:21 +0200

    FIX: removed unnecessary logging

--- runner/runners/slurm.py
+++ runner/runners/slurm.py
@@ -1,22 +1,6 @@
 from runner import BaseRunner
 import subprocess as sb
 from _datetime import datetime
-import logging
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-
-formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
-
-file_handler = logging.FileHandler('sample.log')
-file_handler.setLevel(logging.ERROR)
-file_handler.setFormatter(formatter)
-
-stream_handler = logging.StreamHandler()
-stream_handler.setFormatter(formatter)
-
-logger.addHandler(file_handler)
-logger.addHandler(stream_handler)
 
 
 # List of states from the man page of squeue
